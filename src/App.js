a8a895a1b62627086643b935bedddc22b3096c49
phamvanquy <quypvpy@gmail.com>
2021-11-01 17:18:04 +0700

    phamvanquy 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          Phạm Văn Quỹ
+          Phạm Văn Quỹ 2
         </p>
         <a
           className="App-link"
